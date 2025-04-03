070d020ae34098a2df9a80d042cbcc773131e662
Maksim Sergienko <ififorgetyou@gmail.com>
2025-04-04 00:18:36 +0300

    chore(*): variables and imports

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-import * as fs from "fs"
+import { log } from "console";
+import { readFileSync } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const data = fs.readFileSync("./data.txt");
+const data = readFileSync("./data.txt");
 console.log(data.toString());
+
+// get the resolved path to the file
+const __filename = fileURLToPath(import.meta.url);
+
+// get the name of the directory
+const __dirname = path.dirname(__filename);
+
+log(__filename);
+log(__dirname);
