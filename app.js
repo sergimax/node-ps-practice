d95162f587598f23fbe1824e0dd0b98521f916ae
Maksim Sergienko <ififorgetyou@gmail.com>
2025-04-04 00:45:08 +0300

    chore(*): complete 3rd theme

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
 import { log } from "console";
-import { readFileSync } from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
 
-const data = readFileSync("./data.txt");
-console.log(data.toString());
-
-// get the resolved path to the file
-const __filename = fileURLToPath(import.meta.url);
-
-// get the name of the directory
-const __dirname = path.dirname(__filename);
-
-log(__filename);
-log(__dirname);
+log('hello there!')
